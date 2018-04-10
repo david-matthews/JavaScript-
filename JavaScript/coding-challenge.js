cf0e9d91973a580763c00f2582c8dccf8c5364ed
David Matthews <david.matthews@qa.com>
2018-04-10 17:11:45 +0100

    update 9 with code challenge

---
+++ JavaScript/coding-challenge.js
@@ -0,0 +1,14 @@
+var x = 0;
+
+function square(x) {
+    var y = x * x;
+    document.getElementById("square").append(y);
+}
+
+
+var a,b,c = 0;
+
+function sum(a,b,c) {
+    
+    document.getElementById("sum").append(a+b+c);
+}
